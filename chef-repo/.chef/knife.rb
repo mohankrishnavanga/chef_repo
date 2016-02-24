199562f41d4b6139e55730aff8f048419a78a9c4
mohankrishnavanga <mohankrishnav24@gmail.com>
2016-02-24 15:10:02 +0530

    Chef code

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -3,9 +3,9 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "admin_mk"
-client_key               "#{current_dir}/admin_mk.pem"
+node_name                "mk_admin"
+client_key               "#{current_dir}/mk_admin.pem"
 validation_client_name   "tls-validator"
 validation_key           "#{current_dir}/tls-validator.pem"
-chef_server_url          "https://ec2-54-169-36-51.ap-southeast-1.compute.amazonaws.com/organizations/tls"
+chef_server_url          "https://ec2-54-169-210-187.ap-southeast-1.compute.amazonaws.com/organizations/tls"
 cookbook_path            ["#{current_dir}/../cookbooks"]
--- chef-repo/roles/starter.rb
+++ chef-repo/roles/starter.rb
@@ -2,5 +2,5 @@ name "starter"
 description "An example Chef role"
 run_list "recipe[starter]"
 override_attributes({
-  "starter_name" => "Lone Ranger",
+  "starter_name" => "lone ranger",
 })
